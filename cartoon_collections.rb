0cdeb2af25569dbc5542a4570698fb13ca3718c3
Lucy Wall <lhwall@gmail.com>
2018-03-23 15:53:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(dwarf_names)# code an argument here
   # Your code here
   dwarf_names.each_with_index do |a, i|
     puts "#{i} #{a}"
+  end
 end
 
 def summon_captain_planet# code an argument here
