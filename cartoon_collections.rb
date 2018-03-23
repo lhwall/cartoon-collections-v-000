03ce0b93afd47e709308bbce6874966d8e14b4e8
Lucy Wall <lhwall@gmail.com>
2018-03-23 15:57:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect do |i| i.capit
+  planeteer_calls.collect do |i| i.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
