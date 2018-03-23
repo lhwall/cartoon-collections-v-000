6b65cb50fe83666da0f7af3c97954b9e2ceaec77
Lucy Wall <lhwall@gmail.com>
2018-03-23 17:07:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,6 @@ end
 def long_planeteer_calls(planeteer_calls)# code an argument here
   planeteer_calls.each do |i| if i > 4 return true
 end
-end
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
