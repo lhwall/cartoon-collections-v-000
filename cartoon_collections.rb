bbf37ec2618333dd8c5b2d985603167933bca901
Lucy Wall <lhwall@gmail.com>
2018-03-23 18:14:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   return_value = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheeses.each do |i| if cheese_types.include?[i]
+  cheeses.each do |i| if cheese_types.include?(i)
    return_value = i
    return i
  end
